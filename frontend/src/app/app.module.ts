6fed9d8c134ae6fdc46c22e520027643e62261c0
Nikolas <nikolaspg@hotmail.com>
2021-06-24 16:37:38 -0300

    header-nav-footer

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+//dependencias decomponents prontos do materialDesign do google
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,// components de style
+    MatSidenavModule,// components de style
+    MatListModule// components de style
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
