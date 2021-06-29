67a72a253e0e816357f70bc47d923ba9245306e1
Nikolas <nikolaspg@hotmail.com>
2021-06-29 17:32:53 -0300

    Exibindo Msg De Erro

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,8 +1,9 @@
+import { catchError, map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';// import de métodos http
 import { Injectable } from '@angular/core';
 //snack-bar é um modal de confirmação de criação de produto
 import { MatSnackBar } from '@angular/material/snack-bar'
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { Product } from './product.model'; // modelo do product
 
 @Injectable({
@@ -22,11 +23,14 @@ export class ProductService {
   //passamos uma msg como parametro
   //tudo q está no service conseguimos acessar no component
   //está no product-create
-  showMessage(msg: string): void {
+  //colocamos como param isError como false
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
       verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+      //associamos as class com msgs de success e de erro style.css 
     })
   }
   //Método de criar produto no db
@@ -34,26 +38,49 @@ export class ProductService {
   //q Ele retorna um Observable do Product
   create(product: Product): Observable<Product> {
     //req via post passando o modelo de Product com url  e o product
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
+ 
   //Método de ler q está sendo acessado no product-read.ts
   //Método q acessa a api retornando um Observable de Array de Product
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl)
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
   //metodo para inicializar o componente com items a serrem editados
   readById(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
   //Método para atualizar 
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`;
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
   //método para excluir dentro do db
   delete(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+  //funcao para exibir erro 
+  errorHandler(e: any): Observable<any> {
+    //showMessage(msg, isError = true) se tiver algum durante a req
+    // vai mostra essa msg
+    this.showMessage('Ocorreu um erro !', true)
+    return EMPTY
   }
 }
